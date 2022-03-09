5ca37e20d7fe20728390275548ee41328cb5fc07
pepe lopez <plopez741@gmail.com>
2022-03-09 15:16:10 -0800

    added the functions needed to render the animal

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", () =>{
-    fetch('https://zoo-animal-api.herokuapp.com/animals/rand')
+    fetch('https://zoo-animal-api.herokuapp.com/animals/rand/6')
     .then(resp => resp.json())
-    .then(data => console.log(data))
+    .then(data => {console.log(data)
+        //iterateAnimalArray(data)
+    })
 })
+
+
+function iterateAnimalArray(animalArray){
+    //iterates through the array of animals given from the json
+    animalArray.forEach( animalObject => {
+        renderAnimal(animalObject)
+    });
+}
+
+function renderAnimal(animalObject){
+    //creates the div for the animal and all its coresponding elements
+}
